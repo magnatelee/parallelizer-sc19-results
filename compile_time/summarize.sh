bd30bbe7052f471952a2519b073865a1285386f9
Wonchan Lee <wonchan@cs.stanford.edu>
2019-08-29 08:27:24 +0200

    Add compile time for SpMV

--- compile_time/summarize.sh
+++ compile_time/summarize.sh
@@ -7,12 +7,18 @@ TIME_INFER=`for t in "$TASKS"; do grep "$t" "$1"/compile_auto | grep parallelize
 TIME_SOLVE=`grep solve "$1"/compile_auto | head -1 | awk '{print $5}'`
 TIME_REWRITE=`grep rewrite "$1"/compile_auto | head -1 | awk '{print $5}'`
 TIME_TOTAL=`grep elapsed "$1"/compile_auto | awk '{print $1}' | sed s/"user"//g`
-TIME_TOTAL_MANUAL=`grep elapsed "$1"/compile_manual | awk '{print $1}' | sed s/"user"//g`
+if [ -e "$1"/compile_manual ]; then
+  TIME_TOTAL_MANUAL=`grep elapsed "$1"/compile_manual | awk '{print $1}' | sed s/"user"//g`
+fi
 TIME_CODEGEN=`ruby -e "puts $TIME_TOTAL - $TIME_INFER - $TIME_SOLVE - $TIME_REWRITE"`
 
 echo "$TIME_INFER" | awk '{printf "inference: %.1fms\n", $1*1000}'
 echo "$TIME_SOLVE" | awk '{printf "solve: %.1fms\n", $1*1000}'
-echo "$TIME_REWRITE""s" | awk '{printf "rewrite: %.1fs\n", $1}'
+echo "$TIME_REWRITE""s" | awk '{printf "rewrite: %.1fms\n", $1*1000}'
 echo "$TIME_CODEGEN""s" | awk '{printf "codegen: %.1fs\n", $1}'
 echo "$TIME_TOTAL""s" | awk '{printf "total: %.1fs\n", $1}'
-echo "$TIME_TOTAL_MANUAL""s" | awk '{printf "total (manual): %.1fs\n", $1}'
+if [ -e "$1"/compile_manual ]; then
+  echo "$TIME_TOTAL_MANUAL""s" | awk '{printf "total (manual): %.1fs\n", $1}'
+else
+  echo "total (manual): N/A"
+fi
